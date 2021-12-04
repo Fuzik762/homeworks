81cc6cd5ab937fc42c9f501641e2a9093b731996
Fuzik762 <mikoq762@gmail.com>
2021-12-04 23:51:26 +0300

    0. first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,22 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+    let d = Math.pow(b,2) - 4 * a * c;
+    if(d < 0) {
+      arr = [];
+    } else if(d === 0){
+      arr[0] = -b / (2 * a);
+    } else {
+      arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+      arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+    }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  
 
   return totalAmount;
 }
