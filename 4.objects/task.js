bdfc21f9fc05e32297e0dfbc7033666c0e1ca499
Fuzik762 <mikoq762@gmail.com>
2021-12-05 13:49:53 +0300

    4. tasks done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...Marks) {
+  if(this.marks === undefined){ 
+    this.marks = [...Marks]; 
+    } else {
+      this.marks.push(...Marks);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let result;
+  result = 0;  
+  this.marks.forEach(element => {
+      result+=element;
+  });
+  return result / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+  console.log(this.exclude);
+}
+
