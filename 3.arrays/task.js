e621a45c9e64c62f76674178ac415a309103f7eb
Fuzik762 <mikoq762@gmail.com>
2021-12-05 11:26:19 +0300

    3. tasks done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+      if(arr1.length === arr2.length){
+      result = arr1.every((el, idx) => el === arr2[idx]);
+      } else {
+        result = false;
+      }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(el => el > 0).filter(el => el % 3 === 0).map(el => el * 10);
 
   return resultArr; // array
 }
